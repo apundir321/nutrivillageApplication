037c62803078f1de0daa190df3cea6edd318ad9f
RohtashSingh-Flybunch <rohtash.singh@flybunch.com>
2021-12-02 13:38:09 +0530

    "update product model"

--- src/main/java/com/nurtivillage/java/nutrivillageApplication/model/Product.java
+++ src/main/java/com/nurtivillage/java/nutrivillageApplication/model/Product.java
@@ -15,4 +15,32 @@ public class Product {
     @Column(columnDefinition="INT(1) default 0 COMMENT '1 for highlighter, 0 for remove form highlighter'");
     private int highligter;
     private String image;
+
+    public Product(){
+
+    }
+
+    public Product(String brand,int highlighter,String image,String name){
+        this.brand = brand;
+        this.highligter = highlighter;
+        this.image = image;
+        this.name = name;
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+    public int getHighligter() {
+        return highligter;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getImage() {
+        return image;
+    }
+    public String getName() {
+        return name;
+    }
 }
