4259a6d51b659c3831fa3fa57b8bdf3ef53e8afd
Akash Sharma <Akash.sharma@flybunch.com>
2022-03-31 19:57:05 +0530

    created offer entity

---
+++ src/main/java/com/nurtivillage/java/nutrivillageApplication/service/OfferService.java
@@ -0,0 +1,80 @@
+package com.nurtivillage.java.nutrivillageApplication.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nurtivillage.java.nutrivillageApplication.dao.OfferRepository;
+import com.nurtivillage.java.nutrivillageApplication.model.Offer;
+
+@Service
+public class OfferService {
+@Autowired
+private OfferRepository offerRepository;
+
+// to add offer 
+public Offer addOffer(Offer offer) {
+	try {
+		return offerRepository.save(offer);
+	}
+	catch(Exception e) {
+		throw e;
+	}
+}
+
+// to get offer by offer id
+public Offer getOfferById(Long id) throws Exception {
+	try {
+		 Optional<Offer> offerTemp=offerRepository.findById(id);
+		 if(offerTemp.isPresent()) {
+			 return offerTemp.get();
+		 }
+		 else {
+			 throw new Exception("Offer doesn't exists in DB with this provided Id");
+		 }
+	}
+	catch(Exception e) {
+		throw e;
+	}
+}
+
+//to get List of offer by product id
+public List<Offer> getOffersByProduct(Long productId){
+	try {
+		return offerRepository.findByProductIdAndDeletedAtIsNull(productId);
+	}
+	catch(Exception e) {
+		throw e;
+	}
+}
+
+public List<Offer> getOffersByCategory(Long categoryId){
+	try {
+		return offerRepository.findByCategoryIdAndDeletedAtIsNull(categoryId);
+	}
+	catch(Exception e) {
+		throw e;
+	}
+}
+public Offer deleteOffer(Long offerId) {
+	try {
+		Offer offer=offerRepository.findByIdAndDeletedAtIsNull(offerId);
+	   offer.setDeletedAt(new Date());
+	   return offerRepository.save(offer);
+	}
+	catch(Exception e) {
+		throw e;
+	}
+}
+public Offer updateOffer(Offer offer) {
+	try {
+		return offerRepository.save(offer);
+	}
+	catch(Exception e) {
+		throw e;
+	}
+}
+}
